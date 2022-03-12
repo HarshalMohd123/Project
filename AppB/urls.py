e1fafb38671ba5f21d3a18f27f30e518fa69d7f4
HarshalMohd123 <kingharshalpp@gmail.com>
2022-03-12 11:35:06 +0530

    changes

--- AppB/urls.py
+++ AppB/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-   path('python',views.function),
-   path('',views.function1),
-   path('sample',views.function2)
+   path('',views.function)
 ]
--- AppB/views.py
+++ AppB/views.py
@@ -5,10 +5,4 @@ def AppC(request):
     return HttpResponse('heloo')
 
 def function(request):
-    return render(request,'python.html')
-
-def function1(request):
     return render(request,'facebook login page.html')
-
-def function2(request):
-    return render(request,'sample.html')
